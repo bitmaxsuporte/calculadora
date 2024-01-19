8a74af22749f2eb2655af6a562dc3dbbe0e463e6
bitmaxsuporte <47922508+bitmaxsuporte@users.noreply.github.com>
2024-01-19 15:56:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const baseCosts = [
-    { item: "Cimento CP-V ARI (saco)", unid: "un", quant: 10.00 },
-    { item: "Areia", unid: "m³", quant: 0.50 },
-    { item: "Brita", unid: "m³", quant: 0.50 },
-    { item: "Aço CA-50 - 12,5mm", unid: "kg", quant: 77.0 },
-    { item: "Aço CA-50 - 4,2mm", unid: "kg", quant: 7.85 }
+  { item: "Cimento CP-V ARI (saco)", unid: "un", quant: 10.00 },
+  { item: "Areia", unid: "m³", quant: 0.50 },
+  { item: "Brita", unid: "m³", quant: 0.50 },
+  { item: "Aço CA-50 - 12,5mm", unid: "kg", quant: 77.0 },
+  { item: "Aço CA-50 - 4,2mm", unid: "kg", quant: 7.85 }
+];
 
-  ];
+function calculateCost() {
+  const volumeInput = document.getElementById('volume').value;
+  const volume = parseFloatLocale(volumeInput);
   
-  function calculateCost() {
-    const volume = parseFloat(document.getElementById('volume').value);
-    if (isNaN(volume) || volume <= 0) {
-      alert('Por favor, insira um volume válido.');
-      return;
-    }
-  
-    let resultText = '';
-  
-    baseCosts.forEach((cost) => {
-      const quant = cost.quant * volume;
-      resultText += `\n\n${cost.item}: Qnt: ${quant.toFixed(2)} ${cost.unid}`;
-    });
-  
-    
-    document.getElementById('result').value = resultText;
+  if (isNaN(volume) || volume <= 0) {
+    alert('Por favor, insira um volume válido.');
+    return;
   }
-  
+
+  let resultText = '';
+
+  baseCosts.forEach((cost) => {
+    const quant = cost.quant * volume;
+    resultText += `\n\n${cost.item}: Qnt: ${quant.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} ${cost.unid}`;
+  });
+
+  document.getElementById('result').value = resultText;
+}
+
+function parseFloatLocale(value) {
+  // Remove qualquer espaço em branco
+  value = value.replace(/\s/g, '');
+  // Substitui vírgulas por pontos (se houver vírgulas como separadores decimais)
+  value = value.replace(/,/g, '.');
+  // Converte para float
+  return parseFloat(value);
+}
